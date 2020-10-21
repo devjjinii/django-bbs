d493f97ba3b08671e8c7383a4b255b5c87658945
devjjinii <devjjinii@naver.com>
2020-10-21 22:53:19 +0900

    add form

--- bbs/views.py
+++ bbs/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from bbs.forms import Form
+
 
 # Create your views here.
+def write(request):
+    form = Form()
+    return render(request, 'write.html', {'form': form})
